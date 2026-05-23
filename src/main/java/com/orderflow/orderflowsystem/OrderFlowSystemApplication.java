a7951bc380cda32c35059a23e22e6f50c65963e2
gmozart <mozart.gleison@gmail.com>
2026-05-23 20:17:03 -0300

    chore: initial project structure

---
+++ src/main/java/com/orderflow/orderflowsystem/OrderFlowSystemApplication.java
@@ -0,0 +1,13 @@
+package com.orderflow.orderflowsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFlowSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderFlowSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orderflow/orderflowsystem/OrderFlowSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderflow.orderflowsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderFlowSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
