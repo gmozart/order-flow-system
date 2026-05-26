d682adc7eccbc829d4b8e85683a9358c338833cc
gmozart <mozart.gleison@gmail.com>
2026-05-26 10:15:57 -0300

    feat: Implemented User entity and Repository

---
+++ src/main/java/com/orderflow/orderflowsystem/domain/user/User.java
@@ -0,0 +1,43 @@
+package com.orderflow.orderflowsystem.domain.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+}
