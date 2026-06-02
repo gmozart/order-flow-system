4689ed281797ac681b17f9db835a78eacbd62182
gmozart <mozart.gleison@gmail.com>
2026-06-02 16:25:53 -0300

    feat: Configuring Spring Profiles with Docker

--- src/test/java/com/orderflow/orderflowsystem/OrderFlowSystemApplicationTests.java
+++ src/test/java/com/orderflow/orderflowsystem/OrderFlowSystemApplicationTests.java
@@ -2,12 +2,10 @@ package com.orderflow.orderflowsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class OrderFlowSystemApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
